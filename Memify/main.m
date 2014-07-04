197fef16b2ef163f4b4ac08a6cebcdc64c812865
Ryan Sickles <rsickles@andrew.cmu.edu>
2014-07-04 00:05:23 -0400

    Initial Commit

---
+++ Memify/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Memify
+//
+//  Created by Ryan Sickles on 7/4/14.
+//  Copyright (c) 2014 sickles.ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RSAppDelegate class]));
+    }
+}
